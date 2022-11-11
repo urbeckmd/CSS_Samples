ab4c6bcb76952f621bd6c2d617e6bbe5343c0136
urbeckmd <matthew.urbeck@slu.edu>
2022-11-10 19:01:32 -0600

    add day4

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import Day1 from "./components/Day1";
 import Day2 from "./components/Day2";
 import Day3 from "./components/Day3";
+import Day4 from "./components/Day4";
 
 function App() {
-  const sampleArray = [<Day1 />, <Day2 />, <Day3 />];
+  const sampleArray = [<Day1 />, <Day2 />, <Day3 />, <Day4 />];
 
   return (
     <div className="app">
---
+++ src/components/Day4.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Day4.css";
+
+function Day4() {
+  return (
+    <div class="frame__day4">
+      <div class="center__day4">
+        <div class="dot-1__day4"></div>
+        <div class="dot-2__day4"></div>
+        <div class="dot-3__day4"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Day4;
