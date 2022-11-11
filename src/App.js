b1a3d54c650688a3c049c31b0267c90247fee27f
urbeckmd <matthew.urbeck@slu.edu>
2022-11-10 19:41:43 -0600

    add day16

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import Day2 from "./components/Day2";
 import Day3 from "./components/Day3";
 import Day4 from "./components/Day4";
 import Day6 from "./components/Day6";
+import Day16 from "/components/Day16";
 
 function App() {
-  const sampleArray = [<Day1 />, <Day2 />, <Day3 />, <Day4 />, <Day6 />];
+  const sampleArray = [<Day1 />, <Day2 />, <Day3 />, <Day4 />, <Day6 />, <Day16 />];
 
   return (
     <div className="app">
